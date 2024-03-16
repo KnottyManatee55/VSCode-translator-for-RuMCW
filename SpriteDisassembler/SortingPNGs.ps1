a12a6d567c5eb55eb2f198e7f9f3263dc49d0252
Secret-Rabbit <dimakiselev519844@gmail.com>
2024-03-16 17:51:46 +0300

    Adding a file sorter, thumbnails removed

---
+++ SpriteDisassembler/SortingPNGs.ps1
@@ -0,0 +1,21 @@
+# Раскладывает файлы с расширением .png по папкам с заданным количеством элементов в алфовитном порядке
+$NumberItemsInFolder = 50
+$PrefixFolder = "Part"
+# таблица всех файлов в с расширением.png
+$PngsList = (Get-ChildItem -Name -Path ./*.png)
+# обнуление количества папок
+$NumFolder = 0
+# пеечислениее всех файлов из $PngsList
+for ($i = 0; $i -lt $PngsList.Count; $i++) {
+	# создание папок по количеству сортируемых файлов
+	if ($i % $NumberItemsInFolder -eq 0) {
+		# создание папок
+		$NumFolder++
+		$NameFolder = $PrefixFolder + " " + $NumFolder
+		New-Item -Path . -Name $NameFolder -ItemType "directory"
+	}
+	# создание пути файла
+	$TransferFileName = "./" + $PngsList[$i]
+	# копирование файла в нужную папку
+	Move-Item -Path $TransferFileName -Destination $NameFolder
+}
