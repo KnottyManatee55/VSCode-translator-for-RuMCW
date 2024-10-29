6bef48cc8349222d1bf2cce2df929e2eaaa9f3fa
Secret-Rabbit <dimakiselev519844@gmail.com>
2024-10-29 22:09:46 +0300

    Updating the JSON Localization Table Generator with renaming to MC Localization Table Generator

---
+++ MCJSONLocalizationTableGenerator/BuildTable.py
@@ -0,0 +1,222 @@
+from zipfile import ZipFile
+import requests, os, json, re
+
+# The version of the game. If left empty, the last snapshot will be used
+# Версия игры. Если оставить пустым, будет использоваться последний снапшот
+mc_ver = "1.21.3"
+# In-Game Locale Code
+# Код локализации в игре
+lang = "ru_ru"
+
+# Названия столбцов таблицы по порядку
+# Names of table columns in order
+translate_key = "Ключ перевода"
+en_lang_name = "Англоязычное название"
+localized_name = "Русскоязычное название"
+void_placeholder = "{{Нет}}"
+
+# Allow list
+# Разрешающий список
+favorites_keys = {
+    "^biome.",
+    "^block.",
+    "^color.",
+    "^effect.",
+    "^gamerule.",
+    "advancements.",
+    "container.",
+    "enchantment.minecraft.",
+    "entity.minecraft.",
+    "filled_map.",
+    "flat_world_preset.",
+    "gameMode.",
+    "generator.",
+    "item.minecraft.",
+    "jukebox_song.",
+    "painting.minecraft.",
+    "trim_pattern",
+    "^dataPack.",
+}
+# Restricted list
+# Запрещающий список
+unwanted_keys = {
+    "^advancements.empty",
+    "^advancements.progress$",
+    "^advancements.sad_label$",
+    "^advancements.toast.",
+    "^block.minecraft.beacon.",
+    "^block.minecraft.bed/.",
+    "^block.minecraft.player_head.named$",
+    "^block.minecraft.spawn.not_valid$",
+    "^block.minecraft.spawner.desc1$",
+    "^block.minecraft.spawner.desc2$",
+    "^container.beehive.bees$",
+    "^container.beehive.honey$",
+    "^container.enchant.$",
+    "^container.enchant.clue$",
+    "^container.enchant.lapis.",
+    "^container.enchant.level.many$",
+    "^container.enchant.level.one$",
+    "^container.enchant.level.requirement$",
+    "^container.isLocked$",
+    "^container.repair.cost$",
+    "^container.shulkerBox.itemCount$",
+    "^container.shulkerBox.more$",
+    "^container.shulkerBox.unknownContents$",
+    "^container.spectatorCantOpen$",
+    "^container.upgrade.error_tooltip$",
+    "^container.upgrade.missing_template_tooltip$",
+    "^effect.duration.infinite$",
+    "^effect.none$",
+    "^entity.minecraft.falling_block_type$",
+    "^filled_map.id$",
+    "^filled_map.level$",
+    "^filled_map.locked$",
+    "^filled_map.scale$",
+    "^item.minecraft.bundle.empty$",
+    "^item.minecraft.bundle.fullness$",
+    "^item.minecraft.crossbow.projectile$",
+    "^item.minecraft.debug_stick.empty$",
+    "^item.minecraft.debug_stick.select$",
+    "^item.minecraft.debug_stick.update$",
+    "^item.minecraft.firework_rocket.flight$",
+    "^item.minecraft.smithing_template.applies_to$",
+    "^item.minecraft.smithing_template.upgrade$",
+    "^dataPack.title$",
+    "^dataPack.validation.",
+    "^flat_world_preset.unknown$",
+    "^gameMode.changed$",
+    "^selectWorld.gameMode.adventure.",
+    "^selectWorld.gameMode.creative.",
+    "^selectWorld.gameMode.hardcore.",
+    "^selectWorld.gameMode.spectator.",
+    "^selectWorld.gameMode.survival.",
+}
+
+# Getting links to JSON versions list
+# Получение ссылок на JSON версий
+version_manifest = requests.get(
+    "https://piston-meta.mojang.com/mc/game/version_manifest.json"
+).json()
+# Checking if the version is specified. If no version is specified, the latest snapshot will be used
+# Проверка указания версии. Если версия не задана, то будет использоваться последний снапшот
+if mc_ver == "":
+    mc_ver = version_manifest["latest"]["snapshot"]
+for i in version_manifest["versions"]:
+    if i["id"] == mc_ver:
+        # Reading a version-related JSON file and then converting JSON strings into a Python object
+        # Чтение связанного с версией JSON-файла с последующим преобразованием строк JSON в объект Python
+        ver_json = requests.get(i["url"]).json()
+        # Downloading the client of target version
+        # Скачивание клиента целевой версии
+        client_jar_raw = requests.get(ver_json["downloads"]["client"]["url"])
+        # Saving a version archive named client.zip
+        # Сохранение архива версии с названием client.zip
+        with open("client.zip", "wb") as file:
+            file.write(client_jar_raw.content)
+        # Открытие архива client.zip
+        # Opening the client.zip archive
+        with ZipFile("client.zip", "r") as jar_file:
+            # Открытие файла en_us.json
+            # Opening en_us.json file
+            with jar_file.open("assets/minecraft/lang/en_us.json", "r") as en_data:
+                # Преобразование строк JSON в объект Python
+                # Converting JSON strings to a Python object
+                en_us = json.load(en_data)
+        # Удаление архива client.zip
+        # Remove client.zip archive
+        os.remove("client.zip")
+        # Чтение JSON активов целевой версии с последующим преобразованием строк JSON в объект Python
+        # Reading assets JSON of target version and then converting the JSON strings into a Python object
+        asset_index = requests.get(ver_json["assetIndex"]["url"]).json()
+        # Получение хеша json-файла локализации для заданного языка
+        # Get hash of localization json file for specified language
+        hash_lang = asset_index["objects"]["minecraft/lang/" + lang + ".json"]["hash"]
+        # Чтение JSON-файла локализации целевой версии с последующим преобразованием строк JSON в объект Python
+        # Reading target version localization JSON file and then converting the JSON strings into a Python object
+        other_lang = requests.get(
+            "https://resources.download.minecraft.net/"
+            + hash_lang[:2]
+            + "/"
+            + hash_lang
+        ).json()
+
+
+with open(
+    lang + "-" + mc_ver + ".mediawiki", "w", encoding="utf8"
+) as localization_table:
+    # Запись шапки таблицы в файл
+    # Write table header to file
+    localization_table.write(
+        '{|class="wikitable sortable"\n!'
+        + translate_key
+        + "\n!"
+        + en_lang_name
+        + "\n!"
+        + localized_name
+    )
+    # Перечисление англоязычных ИД`ы
+    # Listing of English-language IDs
+    for i in en_us:
+        # Сброс срабатывания фильтра плохих ключей
+        # Reset bad key filter triggering
+        ex_key = 0
+        # Перечисление плохих ключей
+        # Listing bad keys
+        for u in unwanted_keys:
+            # Проверка ключа на несоответствие
+            # Checking key for inconsistency
+            if bool(re.search(u, i)):
+                # Вызов срабатывания фильтра плохих ключей
+                # Triggering bad key filter
+                ex_key = 1
+        # Перечисление избранных ключей
+        # Listing your favorite keys
+        for f in favorites_keys:
+            # Проверка срабатывания фильтра ключей и проверка на избранность
+            # Checking the activation of key filter and checking for favorites
+            if ex_key == 0 and bool(re.search(f, i)):
+                # Проверка на то, является ли строка описанием
+                # Checking whether the string is a description
+                if bool(re.search(".description", i)):
+                    # Запись в файл отформатированных строк таблицы, если строка является описанием
+                    # Writing formatted table rows to a file, if the string is a description
+                    localization_table.write(
+                        "\n|-\n|"
+                        + i
+                        + "\n|"
+                        + en_us[i]
+                        + "\n|"
+                        + other_lang.setdefault(i, void_placeholder)
+                    )
+                else:
+                    # Обработка пустых строк
+                    # Processing of empty lines
+                    if other_lang.setdefault(i, void_placeholder) == void_placeholder:
+                        # Запись в файл отформатированных строк таблицы, если строка не является описанием, если перевод не задан
+                        # Writing formatted table rows to a file, if string is not a description, if value is not specified
+                        localization_table.write(
+                            "\n|-\n|"
+                            + i
+                            + "\n|[[:en:"
+                            + en_us[i]
+                            + "|"
+                            + en_us[i]
+                            + "]]\n|{{Нет}}"
+                        )
+                    else:
+                        # Запись в файл отформатированных строк таблицы
+                        # Writing formatted table rows to a file
+                        localization_table.write(
+                            "\n|-\n|"
+                            + i
+                            + "\n|[[:en:"
+                            + en_us[i]
+                            + "|"
+                            + en_us[i]
+                            + "]]\n|[["
+                            + other_lang[i]
+                            + "]]"
+                        )
+    localization_table.write("\n|}")
+    localization_table.close
---
+++ MCJSONLocalizationTableGenerator/MCJSONLocalizationTableGenerator.md
@@ -0,0 +1,47 @@
+## Русский
+
+**MC Localization Table Generator** позволяет сгенерировать таблицу MediaWiki для не англоязычной локализации ванильного Minecraft версии 18w02a и выше.
+
+Для указания версии и изменения языка, а также настройки таблицы отредактируйте файл `BuildTable.py`:
+| Переменная | Описание |
+| - | - |
+| `mc_ver` | Версия игры как в лаунчере. Если оставить пустым, будет использоваться последний снапшот |
+| `lang` | Код локализации в игре. Доступные коды можно узнать на Minecraft Wiki в статье [язык](https://minecraft.wiki/w/Language#Languages) |
+| `translate_key` | Отвечает за название первого столбца таблицы, который содержит ключи перевода |
+| `en_lang_name` | Отвечает за название второго столбца таблицы, который содержит англоязычные названия |
+| `localized_name` | Отвечает за название третьего столбца таблицы, который содержит локализованное название |
+| `void_placeholder` | Отвечает за содержимое ячейки, в том случае, если локализованное название отсутствует |
+| `favorites_keys` | Разрешающий список с использованием регулярных выражений |
+| `unwanted_keys` | Запрещающий список с использованием регулярных выражений |
+
+
+После запуска создаётся файл с названием `<версия_игры>-<код_локализации>.json `, содержащий сортируемую таблица следующего вида:
+| Ключ перевода | Англоязычное название | Русскоязычное название |
+| - | - | - |
+| ... | ... | ... |
+
+Формат вывода предполагает создание ссылок для значений всех ключей перевода, не являющихся описанием.
+
+## English
+
+**MC LocalizationTable Generator** allows you to generate a MediaWiki table for non-English localization of vanilla Minecraft version 18w02a and higher.
+
+To specify the version and change the language, as well as the table settings, edit the file `BuildTable.py `:
+| Variable | Description |
+| - | - |
+| `mc_ver` | The version of the game is like in the launcher. If left blank, the last snapshot will be used |
+| `lang` | The localization code in the game. The available codes can be found on the Minecraft Wiki in the article [language](https://minecraft.wiki/w/Language#Languages) |
+| `translate_key` | Responsible for the name of the first column of the table, which contains the translation keys |
+| `en_lang_name` | Responsible for the name of the second column of the table, which contains English-language names |
+| `localized_name` | Responsible for the name of the third column of the table, which contains the localized name |
+| `void_placeholder` | Responsible for the contents of the cell, if there is no localized name |
+| `favorites_keys` | Allow list using regular expressions |
+| `unwanted_keys` | Restricted list using regular expressions |
+
+
+After launch, a file is created with the name `<game_version>-<localization_code>.json ` containing a sortable table of the following type:
+| English-language name | English-language name | localized name |
+| - | - | - |
+| ... | ... | ... |
+
+The output format assumes the creation of links for the values of all translation keys that are not a description.
